2f205d1dca551561d6af5aab9817656d1754e1af
Tvisher <tvisher@mail.ru>
2022-06-15 16:57:35 +0600

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -95,25 +95,28 @@ const reviewsSlider = new Swiper('.reviews__slider', {
     }
 });
 
-
-
 //логика работы меню бургер
 document.body.addEventListener('click', (e) => {
     const target = e.target;
     if (target.closest('[data-burger-menu]')) {
         target.closest('[data-burger-menu]').classList.toggle('active');
-        document.querySelector('[data-header-menu]').classList.toggle('active');
+        document.querySelector('[data-header-menu]').classList.toggle('show');
         document.body.classList.toggle('hidden');
     }
-    if (target.closest('.fullscreen-slider__desc')) {
-        target.closest('.fullscreen-slider__desc').classList.add('open');
+    if (target.closest('.tour-modal.show') && !target.closest('.tour-modal__content')) {
+        target.closest('.tour-modal.show').classList.remove('show');
+    }
+
+    if (target.closest('.book-modal.show') && !target.closest('.book-modal__wrapper')) {
+        target.closest('.book-modal.show'ы).classList.remove('show');
     }
 });
 
 // Маска на номера телефона
 document.querySelectorAll('input[type="tel"]').forEach(input => {
     const mask = IMask(input, {
-        mask: '+{7}(000) 000-00-00'
+        mask: '+{7}(000) 000-00-00',
+
     });
 });
 
@@ -121,4 +124,31 @@ document.querySelectorAll('input[type="tel"]').forEach(input => {
 $("[data-toggle-btn]").click(function () {
     $(this).parent().toggleClass('open')
     $(this).parent().find("[data-toggle-content]").slideToggle("slow");
+});
+
+
+
+$(function () {
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: 'Предыдущий',
+        nextText: 'Следующий',
+        currentText: 'Сегодня',
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        weekHeader: 'Не',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+    $("#datepicker").datepicker({
+        altFormat: "DD, d MM, yy",
+        minDate: 0
+    });
 });
